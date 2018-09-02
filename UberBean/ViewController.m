3fe43aa4f8966736432e9f514f52990b2d1dab2a
nwainwri <nathanwainwright@gmail.com>
2018-09-02 16:03:10 -0700

    user location now shows, issue was with programmatially adding mapview and such, redid via storyboard

--- UberBean/ViewController.m
+++ UberBean/ViewController.m
@@ -7,25 +7,32 @@
 //
 
 #import "ViewController.h"
-#import <MapKit/MapKit.h> //imports mapkit, need this for displaying map
-#import <CoreLocation/CoreLocation.h> //imports corelocation, need this to get location of phone.
+//#import <MapKit/MapKit.h> //imports mapkit, need this for displaying map
+//#import <CoreLocation/CoreLocation.h> //imports corelocation, need this to get location of phone.
+@import CoreLocation;
+@import MapKit;
 #import "MyAnnotation.h"
-
 
 @interface ViewController () <CLLocationManagerDelegate,MKMapViewDelegate>
 
-@property (strong, nonatomic) CLLocationManager *locationManager;
+//@property (strong, nonatomic) MKMapView *mapView;
 
-@property (strong, nonatomic) MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+
+@property (nonatomic,strong) CLLocationManager *locationManager;
 
 @end
 
 @implementation ViewController
 
+
+
+
+
 - (void)viewDidLoad {
   [super viewDidLoad];
-//  self.mapView.showsUserLocation = YES;
-  
+  //  self.mapView.showsUserLocation = YES;
   
   //  sets up location manager properly, setting desired accuuracy boundaries, distant, and settine delegate for it.
   self.locationManager = [[CLLocationManager alloc] init];
@@ -33,27 +40,37 @@
   self.locationManager.distanceFilter = 20;
   self.locationManager.delegate = self;
   
+  //************
+  //
+  // MARK: I WANT TO KNOW HOW TO ADD PROGRAMMATICALLY
+  //
+  //************
+//  MKMapView *mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
+//  [self.view addSubview:mapView];
+  
+  
+  
   [self.locationManager requestWhenInUseAuthorization];
   
   //setups view to have a map, covering entire screen
-  MKMapView *mapView = [[MKMapView alloc] initWithFrame:self.view.frame];
-  [self.view addSubview:mapView];
   self.mapView.delegate = self;
   self.mapView.showsUserLocation = YES;
   self.mapView.showsPointsOfInterest = YES;
   self.mapView.mapType = MKMapTypeStandard; //MKMapTypeHybridFlyover;
   
-//  [self.mapView registerClass:[MKMarkerAnnotationView class] forAnnotationViewWithReuseIdentifier:@"myAnnotation"];
-//
-//  MyAnnotation *annotation = [[MyAnnotation alloc] init];
-//  annotation.title = @"Something weird here";
-//  annotation.subtitle = @"I dunno what";
-//  annotation.coordinate = CLLocationCoordinate2DMake(40.6892656, -74.0466891);
-//
-//  [self.mapView addAnnotation:annotation];
+
+  
+  //  [self.mapView registerClass:[MKMarkerAnnotationView class] forAnnotationViewWithReuseIdentifier:@"myAnnotation"];
+  //
+  //  MyAnnotation *annotation = [[MyAnnotation alloc] init];
+  //  annotation.title = @"Something weird here";
+  //  annotation.subtitle = @"I dunno what";
+  //  annotation.coordinate = CLLocationCoordinate2DMake(40.6892656, -74.0466891);
+  //
+  //  [self.mapView addAnnotation:annotation];
   
   // got an ERROR: 2018-09-02 14:54:42.619316-0700 UberBean[1088:33226] **** Manager failed: The operation couldn’t be completed. (kCLErrorDomain error 0.)
-
+  
   
 }
 
@@ -79,10 +96,9 @@
   NSLog(@"**** Updated locations %@", locations);
   CLLocation *loc = locations[0];
   [self.mapView setRegion:MKCoordinateRegionMake(loc.coordinate,
-                                                 MKCoordinateSpanMake(0.2, 0.2))
+                                                 MKCoordinateSpanMake(0.03, 0.03))
                  animated:YES];
 }
-
 
 // MARK: - MKMapViewDelegate
 
@@ -103,11 +119,5 @@
   return nil;
 }
 
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
-  // Dispose of any resources that can be recreated.
-}
-
-
 @end
+
